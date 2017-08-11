c3a446206f093b8bfb63250cbd53acaf406ab5d5
Eyden Villanueva <eydenv85@gmail.com>
2017-08-11 14:14:39 -0500

    Creación del Modelo Publicación
    (Aún hace falta agregar una llave foránea)

--- blog/publicaciones/models.py
+++ blog/publicaciones/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=450)
+    contenido = models.TextField()
+    fecha = models.DateField(auto_now=False,auto_now_add=True)
+    titulo_formato = models.CharField(null=True)
+
+    def __str__(self):
+        return self.titulo
