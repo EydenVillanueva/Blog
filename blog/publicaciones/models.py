1f6c900b8890ee38cdcd96708475e2820fd074e1
Eyden Villanueva <eydenv85@gmail.com>
2017-08-11 16:31:10 -0500

    Correción modelo atributo titulo_formato

--- blog/publicaciones/models.py
+++ blog/publicaciones/models.py
@@ -6,7 +6,7 @@ class Publicacion(models.Model):
     titulo = models.CharField(max_length=450)
     contenido = models.TextField()
     fecha = models.DateField(auto_now=False,auto_now_add=True)
-    titulo_formato = models.CharField(null=True)
+    titulo_formato = models.CharField(max_length=80,null=True)
 
     def __str__(self):
         return self.titulo
