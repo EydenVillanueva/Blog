db6481dfb4e4e22f04dbb033d3585f8b7bbfb37d
Eyden Villanueva <eydenv85@gmail.com>
2017-08-15 22:06:49 -0500

    Correción en Urls.py

--- blog/publicaciones/urls.py
+++ blog/publicaciones/urls.py
@@ -6,5 +6,5 @@ from . import views
 app_name = 'publicaciones'
 urlpatterns = [
     url(r'^c-p$', views.crearPublicacion, name='index'),
-    url(r'^(?P<titulo_formateado>[a-zA-Z0-9-]+)$', views.detalle, name='detalle'),
+    url(r'^(?P<titulo_formateado>[a-zA-Z0-9-]+)', views.detalle, name='detalle'),
 ]
